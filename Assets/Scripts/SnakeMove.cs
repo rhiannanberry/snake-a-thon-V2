e39f08f08f32f3d03177e0eea436587a2ec212af
Rhiannan Berry <rhiannanb97@gmail.com>
2017-11-01 22:07:00 -0400

    First commit of snake a thon v2: the snakening

---
+++ Assets/Scripts/SnakeMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMove : MonoBehaviour {
+    public float maxSpeed = 5f;
+    public float acceleration = 0.5f;
+    private float speed = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed = Mathf.Clamp((speed + acceleration), 0f, maxSpeed);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 diff = mousePos - transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        transform.position = Vector2.MoveTowards(transform.position, mousePos, Time.deltaTime * speed);
+	}
+}
