c1a28d1c45cdb28a1384e2dedf29f155172e7f32
rhiannanberry <rhiannanb97@gmail.com>
2017-12-19 11:37:07 -0500

    version change and mucking about with particle fx

---
+++ Assets/Scripts/CursorEffects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorEffects : MonoBehaviour {
+    private float z;
+	// Use this for initialization
+	void Start () {
+        z = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mousexy = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousexy.x, mousexy.y, z);
+    }
+}
